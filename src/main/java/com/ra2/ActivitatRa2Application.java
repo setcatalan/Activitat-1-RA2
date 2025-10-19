b0782839d70866fba59f201778f42c72d60c8255
Set catalan <2024_set.catalan@iticbcn.cat>
2025-10-19 17:21:49 +0200

    first commit

---
+++ src/main/java/com/ra2/ActivitatRa2Application.java
@@ -0,0 +1,13 @@
+package com.ra2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitatRa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitatRa2Application.class, args);
+	}
+
+}
